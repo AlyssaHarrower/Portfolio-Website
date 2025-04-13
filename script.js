a517525af1c1a9af8099466150d97a43d3f2a518
AlyssaHarrower <alyssaharrower@gmail.com>
2025-04-12 19:09:33 -1000

    updated color palette and added new toggle images

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const toggleBtn = document.getElementById('theme-toggle');
+const toggleImg = document.getElementById('theme-toggle-img'); // Image inside the button
 const body = document.body;
 
 // Load saved mode
 if (localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark-mode');
-  toggleBtn.textContent = '☀️';
+  toggleImg.src = 'images/sun-icon.png'; // Light mode icon (shown when in dark mode)
+} else {
+  toggleImg.src = 'images/moon-icon.png';
 }
-
+//Listens for a button click and toggles to light or dark mode accordingly
 toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
-  const isDark = body.classList.contains('dark-mode');
-  toggleBtn.textContent = isDark ? '☀️' : '🌙';
-  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  const isDark = body.classList.contains('dark-mode'); // Sets isDark value
+  toggleImg.src = isDark ? 'images/sun-icon.png' : 'images/moon-icon.png'; // Sets image
+  localStorage.setItem('theme', isDark ? 'dark' : 'light'); // Sets saved mode
 });
