d63f9c037c6020838805f19feb2c8d9cdb33a3b2
AlyssaHarrower <alyssaharrower@gmail.com>
2025-04-14 00:35:29 -1000

    fix

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const body = document.body;
 // Load saved mode
 if (localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark-mode');
-  toggleImg.src = '../images/sun-icon.png'; // Light mode icon (shown when in dark mode)
+  toggleImg.src = './images/sun-icon.png'; // Light mode icon (shown when in dark mode)
 } else {
-  toggleImg.src = '../images/moon-icon.png';
+  toggleImg.src = './images/moon-icon.png';
 }
 //Listens for a button click and toggles to light or dark mode accordingly
 toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   const isDark = body.classList.contains('dark-mode'); // Sets isDark value
-  toggleImg.src = isDark ? '../images/sun-icon.png' : '../images/moon-icon.png'; // Sets image
+  toggleImg.src = isDark ? './images/sun-icon.png' : './images/moon-icon.png'; // Sets image
   localStorage.setItem('theme', isDark ? 'dark' : 'light'); // Sets saved mode
 });
